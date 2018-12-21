3a3f07e841c7fcce3b4412003e58d5b7ede79653
Angela Chou <angnchou@gmail.com>
2018-12-21 11:22:35 -0800

    update seeding script, schema,  and begin dev env set up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Comonent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>Hello</div>
+    }
+}
+
+export default App;
