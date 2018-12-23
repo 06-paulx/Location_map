c5e64352e23599f2103070d2e1c8bc1b6e2ec8d6
Angela Chou <angnchou@gmail.com>
2018-12-22 18:38:04 -0800

    update css font and color

--- client/src/components/Location.jsx
+++ client/src/components/Location.jsx
@@ -5,8 +5,8 @@ const Location = props => (
   <div>
     <h2 className="location">Location</h2>
     <h4 className="city">City, Country</h4>
-    <h4 className="airport">Time to Airport</h4>
-
+    <h4 className="airport">Airport</h4>
+    <p className="time">Time to Airport</p>
     <p className="description">Description</p>
     <Map />
   </div>
