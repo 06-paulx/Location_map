00528e3d5495b6a39de42b79aa60bf75aab828dd
Angela Chou <angnchou@gmail.com>
2018-12-24 18:12:39 -0800

    update seed file and map rendering with custom marker

--- db/index.js
+++ db/index.js
@@ -1,18 +1,32 @@
 const connection = require('./connection.js');
 
-const getCoordinates = (listingId, callback) => {
-  const query =
-    'SELECT longitude, latitude FROM locationMap WHERE listing_id= ?';
+// const getCoordinates = (listingId, callback) => {
+//   const query =
+//     'SELECT longitude, latitude FROM locationMap WHERE listing_id= ?';
+//   const params = [listingId];
+//   connection.query(query, params, (err, result) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, result[0]);
+//     }
+//   });
+// };
+
+const getListing = (listingId, callback) => {
+  const query = 'SELECT * FROM locationMap WHERE listing_id=?';
   const params = [listingId];
   connection.query(query, params, (err, result) => {
+    // console.log(result, 'DB RESULTT');
     if (err) {
       callback(err, null);
     } else {
-      callback(null, result[0]);
+      callback(null, result);
     }
   });
 };
 
 module.exports = {
-  getCoordinates
+  // getCoordinates,
+  getListing,
 };
